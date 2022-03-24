8750314be6d3797aa1da37ec391b0005eb32e310
곽민지 <beinte0419@snu.ac.kr>
2022-03-24 11:07:28 +0900

    [baekjoon] 2447(update)

--- baekjoon/2447.py
+++ baekjoon/2447.py
@@ -1,3 +1,5 @@
+# 풀이 1
+
 pattern = ['***', '* *', '***']
 n = int(input())
 
@@ -18,5 +20,28 @@ def star(n):
 
 
 star(n)
+for p in pattern:
+    print(p)
+
+
+# 풀이 2
+n = int(input())
+
+
+def star(n):
+    if n == 1:
+        return ["*"]
+    pattern = star(n//3)
+    pattern *= 3
+    l = len(pattern)
+    for i in range(l):
+        if i < (l//3) or i > (l//3*2-1):
+            pattern[i] *= 3
+        else:
+            pattern[i] = pattern[i] + ' '*len(pattern[i]) + pattern[i]
+    return pattern
+
+
+pattern = star(n)
 for p in pattern:
     print(p)
