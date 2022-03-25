db2e0427d39c390c3bdb819f8d3b60988938b076
곽민지 <beinte0419@snu.ac.kr>
2022-03-25 14:56:33 +0900

    [baekjoon] 11047

---
+++ baekjoon/11047.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+count = 0
+for coin in reversed(coins):
+    count += k//coin
+    k %= coin
+    if k == 0:
+        break
+print(count)
