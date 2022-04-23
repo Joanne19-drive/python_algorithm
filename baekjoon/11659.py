f8f76e1242ae929911834b497c17e2de5a95f2af
곽민지 <beinte0419@snu.ac.kr>
2022-04-23 20:48:05 +0900

    [baekjoon] 11659

---
+++ baekjoon/11659.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for i in range(1, n):
+    arr[i] += arr[i-1]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    if a == 1:
+        print(arr[b-1])
+    else:
+        print(arr[b-1]-arr[a-2])
